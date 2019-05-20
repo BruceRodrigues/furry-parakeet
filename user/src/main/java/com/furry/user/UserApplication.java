f96264b73305236ee8368b81e6c58c97c743e996
Bruce Rodrigues <brucerodrigues6@gmail.com>
2019-05-19 21:28:44 -0300

    user endpoint config

---
+++ user/src/main/java/com/furry/user/UserApplication.java
@@ -0,0 +1,14 @@
+package com.furry.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+}
