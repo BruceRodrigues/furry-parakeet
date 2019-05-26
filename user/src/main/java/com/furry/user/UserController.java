be988112e54127ecf45a5617549ce11984437ca6
Bruce Rodrigues <brucerodrigues6@gmail.com>
2019-05-26 00:15:16 -0300

    get request + database config

--- user/src/main/java/com/furry/user/UserController.java
+++ user/src/main/java/com/furry/user/UserController.java
@@ -1,11 +1,28 @@
 package com.furry.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.furry.user.model.User;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/user")
+@AllArgsConstructor
 public class UserController {
+
+    private UserRepository repository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> get(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(this.repository.findById(id).get(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public void add(@ModelAttribute("user") User user) {
+        this.repository.save(user);
+    }
+
 
 
 
