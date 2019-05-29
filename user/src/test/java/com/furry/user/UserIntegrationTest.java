2c92e0c99004964e2a0970d427ed0af2f861a1b4
Bruce Rodrigues <brucerodrigues6@gmail.com>
2019-05-28 23:21:57 -0300

    integration test

--- user/src/test/java/com/furry/user/UserIntegrationTest.java
+++ user/src/test/java/com/furry/user/UserIntegrationTest.java
@@ -1,11 +1,7 @@
 package com.furry.user;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.furry.user.model.Endereco;
 import com.furry.user.model.User;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +14,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Arrays;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,8 +24,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@PropertySource("classpath:application.yml")
-@ActiveProfiles("test")
 public class UserIntegrationTest {
 
 	@Autowired
@@ -42,7 +35,6 @@ public class UserIntegrationTest {
 	@Before
 	public void inserts() {
         User user = new User();
-        user.setId(1L);
         user.setName("Name");
         user.setPassword("secret");
         user.setUsername("username");
@@ -59,37 +51,67 @@ public class UserIntegrationTest {
         this.repository.save(user);
 	}
 
-//	@Test
-//	public void testGet() throws Exception {
-//		this.mockMvc.perform(get("/user/1"))
-//			.andExpect(status().isOk())
-//			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-//            ;
-//	}
+	@Test
+	public void testGet() throws Exception {
+		this.mockMvc.perform(get("/user/1"))
+			.andExpect(status().isOk())
+			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(content().string(
+                    "{" +
+                    "\"id\":1," +
+                    "\"name\":\"Name\"," +
+                    "\"password\":\"secret\"," +
+                    "\"username\":\"username\"," +
+                    "\"endereco\":{" +
+                        "\"id\":1," +
+                        "\"coMunicipio\":\"4205407\"," +
+                        "\"noMunicipio\":\"Florianópolis\"," +
+                        "\"sgUf\":\"SC\"," +
+                        "\"nuCep\":\"88034000\"," +
+                        "\"dsLogradouro\":\"Logradouro\"," +
+                        "\"dsComplemento\":\"Complemento\"," +
+                        "\"dsBairro\":\"Bairro\"," +
+                        "\"dsNumero\":\"123\"" +
+                        "}" +
+                    "}"));
+	}
 
 	@Test
     public void testAdd() throws Exception {
-
-	    User test = new User();
-	    test.setId(3L);
-        test.setName("Test");
-        test.setPassword("secret2");
-        test.setUsername("username2");
-
 	    //Test request
 	    this.mockMvc.perform(post("/user/add")
-            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-            .content(asJsonString(test)))
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{" +
+                    "\"name\": \"postman\"," +
+                    "\"password\": \"pass\"," +
+                    "\"username\": \"post\"," +
+                    "\"endereco\": {" +
+                        "\"coMunicipio\": \"123123\"," +
+                        "\"noMunicipio\": \"Florianópolis\"," +
+                        "\"sgUf\": \"SC\"," +
+                        "\"nuCep\": \"36880000\"," +
+                        "\"dsLogradouro\": \"Logr\"," +
+                        "\"dsComplemento\": \"Complemento\"," +
+                        "\"dsBairro\": \"Bairro\"," +
+                        "\"dsNumero\": \"123\"" +
+                        "}" +
+                    "}"))
         .andExpect(status().isOk())
         .andExpect(content().string("Usuário salvo com sucesso"));
-    }
 
-    public static String asJsonString(final Object obj) {
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        Optional<User> user = this.repository.findById(2L);
+        assertThat(user.get()).isNotNull();
+        assertThat(user.get().getName()).isEqualTo("postman");
+        assertThat(user.get().getPassword()).isEqualTo("pass");
+        assertThat(user.get().getUsername()).isEqualTo("post");
+        assertThat(user.get().getEndereco().getCoMunicipio()).isEqualTo("123123");
+        assertThat(user.get().getEndereco().getNoMunicipio()).isEqualTo("Florianópolis");
+        assertThat(user.get().getEndereco().getSgUf()).isEqualTo("SC");
+        assertThat(user.get().getEndereco().getNuCep()).isEqualTo("36880000");
+        assertThat(user.get().getEndereco().getDsLogradouro()).isEqualTo("Logr");
+        assertThat(user.get().getEndereco().getDsComplemento()).isEqualTo("Complemento");
+        assertThat(user.get().getEndereco().getDsBairro()).isEqualTo("Bairro");
+        assertThat(user.get().getEndereco().getDsNumero()).isEqualTo("123");
     }
 
 }
